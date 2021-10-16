9f8a0878d2e21790c93c2a85e6dc4f2913fe7bea
张子枫 <1048411925@qq.com>
2021-10-16 21:51:15 +0800

    Update jd_speed_sign.js

--- jd_speed_sign.js
+++ jd_speed_sign.js
@@ -93,7 +93,6 @@ async function jdGlobal() {
     //await apTaskList()
     //await wheelsHome()
     //if ($.canhelp) {
-      //console.log(`\n京东账号${$.index}开始助力【zero205】邀请有礼，感谢！\n`);
       //await invite()
       //await invite2()
     }
