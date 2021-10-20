03f8e214080d7d4b2618e121d3567ee82f78be0b
张子枫 <1048411925@qq.com>
2021-10-20 19:30:19 +0800

    Update rush_lzdz1_huiju.js

--- rush_lzdz1_huiju.js
+++ rush_lzdz1_huiju.js
@@ -28,8 +28,6 @@ if ($.isNode()) {
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
         return;
     }
-    authorCodeList = await getAuthorCodeList('')
-    if(authorCodeList === '404: Not Found'){
         authorCodeList = [
             '',
         ]
