cc21893bbbe8c11bd33a181cc911d1c2f427d64e
张子枫 <1048411925@qq.com>
2021-12-04 17:59:45 +0800

    Create jd_task_checkCookie.js

---
+++ jd_task_checkCookie.js
@@ -0,0 +1,16 @@
+cron:50 7-23 * * *
+const $ = new Env('京东cookie检测');
+!(async () => {
+        let filename = process.mainModule.filename.match(/(\w+).js/)[1]
+        let dirname = process.mainModule.path
+        let type = filename.split('_')[0]
+        if (['js', 'jx', 'jr', 'jw'].includes(type)) {
+            type = 'jd'
+        }
+        let main = require(`${dirname}/parse/${type}/${filename}`)
+        let a = new main()
+        await a.init({})
+    }
+)().catch((e) => {
+    console.log(e.message)
+})
