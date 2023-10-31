913882eb1960898f6a8bfef42965b03467b2d85f
张子枫 <1048411925@qq.com>
2023-11-01 03:09:52 +0800

    fix

--- jd_fruit_watering.js
+++ jd_fruit_watering.js
@@ -7,7 +7,7 @@
 
 */
 require("global-agent/bootstrap");
-global.GLOBAL_AGENT.HTTP_PROXY="http://172.17.0.1:54987";
+global.GLOBAL_AGENT.HTTP_PROXY="http://172.17.0.1:9876";
 global.GLOBAL_AGENT.NO_PROXY='*.ailoveu.eu.org,*.kingran.*,3.cn,*.telegram.org,github.com,*.foo.com,*.baz.com,cdn.nz.lu,127.0.0.1,*.feverrun.*,hailiangip.*,*.nolanstore.*,*.jsdelivr.*,xgzq.ml,hub.llll.host,*.moxigame.*,*.feverrun.*,172.17.0.1'
 const $ = new Env('东东农场快速浇水')
 const jdCookie = require('./jdCookie')
