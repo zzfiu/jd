c950def0be000c997fbafc4a44e492ffa82a9ffc
张子枫 <1048411925@qq.com>
2023-12-29 20:49:56 +0800

    fix

--- jd_speed_sign.js
+++ jd_speed_sign.js
@@ -1,10 +1,8 @@
 /*
-cron "0 6,13,19 * * *" jd_speed_sign_Mod.js, tag:京东特价版任务
+cron "5 3,15 * * *" jd_speed_sign.js, tag:京东特价版任务
 */
 //详细说明参考 https://github.com/ccwav/QLScript2.
-require("global-agent/bootstrap");
-global.GLOBAL_AGENT.HTTP_PROXY="http://172.17.0.1:9876";
-global.GLOBAL_AGENT.NO_PROXY='*.ailoveu.eu.org,*.kingran.*,3.cn,*.telegram.org,github.com,*.foo.com,*.baz.com,cdn.nz.lu,127.0.0.1,*.feverrun.*,hailiangip.*,*.nolanstore.*,*.jsdelivr.*,xgzq.ml,hub.llll.host,*.moxigame.*,*.feverrun.*,172.17.0.1'
+
 const $ = new Env('京东特价版任务');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
@@ -86,9 +84,9 @@ async function jdGlobal(cookie,url_uuid) {
 	if (!isLogin)
 		return;
 	
-    await wheelsHome(cookie)
+    //await wheelsHome(cookie)
     await apTaskList(cookie)
-    await wheelsHome(cookie)
+    //await wheelsHome(cookie)
     await taskList(cookie,url_uuid)
     if (llAPIError) {      
       return;
